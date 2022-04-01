1d0ccd022eb597cdfd4221973e722c323168c02e
HyunjaePark <hyunjae@HyunjaeParkui-MacBookPro.local>
2022-04-01 20:31:33 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const [tab, setTab] = useState("KRW");
+	// const tabs = ["원화 마켓", "BTC 마켓"]; // KRW, BTC
+
+	// const renderTabs = () => {
+	// 	return tabs.map((v, i) => {
+	// 		return (
+	// 			<div key={i + "header"}>
+	// 				<input
+	// 					type="button"
+	// 					id="header"
+	// 					name="header"
+	// 					onClick={(e) => moveTab(e)}
+	// 					value={v}
+	// 				/>
+	// 			</div>
+	// 		);
+	// 	});
+	// };
+
+	// const moveTab = (e) => {
+	// 	console.log(e);
+	// };
+
+	const renderChart = () => {
+		return <div className={styles.header_chart}>KRW list 5개 맵돌리기</div>;
+	};
+
+	return (
+		<div className={styles.App}>
+			<header className={styles.header}>
+				<div className={styles.header_title}>
+					<p>{"마켓 변동률 TOP5"}</p>
+				</div>
+				{renderChart()}
+			</header>
+		</div>
+	);
 }
 
 export default App;
